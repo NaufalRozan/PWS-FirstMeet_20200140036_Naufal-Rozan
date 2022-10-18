b3b7a51b9dfd49e3367ff86270d993672fecc4ae
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2022-10-18 16:08:56 +0700

    Tugas PWS 1

---
+++ src/main/java/pws1/pws1/Pws1Application.java
@@ -0,0 +1,13 @@
+package pws1.pws1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pws1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pws1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pws1/pws1/Pws1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pws1.pws1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pws1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
